e04e9ace57e0dbb4953aad523829cb16b95dcc7b
Laptop Asus <stipro150197@gmail.com>
2022-07-05 11:08:00 -0500

    Probando

--- consultaProductos.php
+++ consultaProductos.php
@@ -1,21 +1,54 @@
 <?php
-/* header('Content-type: application/json; charset=utf-8'); */
-$curl = curl_init();
+header('Content-type: application/json; charset=utf-8');
+$rptController = array();
+if ($_POST) {
+  $arrayForm = json_decode($_POST['data'], true);
+  $curl = curl_init();
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => 'https://www.promart.pe/api/catalog_system/pub/products/search/?sc=2&ft=escoba&O=OrderByScoreDESC',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'GET',
+  ));
 
-curl_setopt_array($curl, array(
-  CURLOPT_URL => 'https://www.promart.pe/api/catalog_system/pub/products/search/?sc=2&ft=escoba&O=OrderByScoreDESC',
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'GET',
-));
+  $response = curl_exec($curl);
 
-$response = curl_exec($curl);
+  curl_close($curl);
+  // Procesamos STRING a JSON
+  $val_JsonEncode =  json_decode($response);
+  echo json_encode($val_JsonEncode);
+} elseif ($_GET) {
 
-curl_close($curl);
-var_dump($response);
-/* echo json_encode($response); */
-?>
+  $curl = curl_init();
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => 'https://www.promart.pe/api/catalog_system/pub/products/search/?sc=2&ft=escoba&O=OrderByScoreDESC',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'GET',
+  ));
+
+  $response = curl_exec($curl);
+
+  curl_close($curl);
+  // Procesamos STRING a JSON
+  $val_JsonEncode =  json_decode($response);
+  $final_Json = json_encode($val_JsonEncode);
+  $rptController = array(
+    "estado" => '1',
+    "rptController" => $final_Json
+  );
+} else {
+  $rptController = array(
+    "codigoEstado" => '0',
+    "rptController" => 'no se recibio datos'
+  );
+}
+echo json_encode($rptController);
